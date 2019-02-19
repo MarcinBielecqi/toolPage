82732becd2004dc85f39645a899091c9f6fd1120
Marcin Bielecqi <marcin.bielecki2804@gmail.com>
2019-02-19 14:27:04 +0000

    first merge

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"TestApp/TestApp/test/unit/controller/Master.controller"
+], function () {
+	"use strict";
+});
---
+++ webapp/view/Master.view.js
@@ -0,0 +1,70 @@
+sap.ui.jsview("TestApp.TestApp.view.Master", {
+
+	/** Specifies the Controller belonging to this View. 
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf controller.Master
+	 */
+	getControllerName: function () {
+		return "TestApp.TestApp.controller.Master";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	 * Since the Controller is given to this method, its event handlers can be attached right away.
+	 * @memberOf controller.Master
+	 */
+
+	oToolPage: null,
+	oToolHeader: null,
+	oPage: null,
+	oSideNavigation: null,
+	oNavigationList: null,
+	oNavigationListItem: null,
+
+	createContent: function (oController) {
+
+		this.oNavigationListItem = new sap.tnt.NavigationListItem({
+			items: {
+				path: "side>/navigation",
+				template: new sap.tnt.NavigationListItem({
+					key: "{side>key}",
+					text: "{side>title}",
+					expanded: "{side>expanded}",
+					icon: "{side>icon}"
+				})
+			},
+			templateShareable: false
+		});
+
+		this.oNavigationList = new sap.tnt.NavigationList({
+			items: {
+				path: "side>/navigation",
+				template: new sap.tnt.NavigationListItem({
+					key: "{side>key}",
+					text: "{side>title}",
+					expanded: "{side>expanded}",
+					icon: "{side>icon}"
+				})
+			}
+		});
+
+		this.oPage = new sap.m.Page({
+			title: "{i18n>title}",
+			content: []
+		});
+		this.oSideNavigation = new sap.tnt.SideNavigation({
+			item: this.oNavigationList
+		});
+		this.oToolHeader = new sap.tnt.ToolHeader("toolHeader", {
+			content: [new sap.m.Button()]
+		});
+
+		this.oToolPage = new sap.tnt.ToolPage("app", {
+			header: this.oToolHeader,
+			mainContents: this.oPage,
+			sideContent: this.oSideNavigation
+		});
+
+		return this.oToolPage;
+	}
+
+});
